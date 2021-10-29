82c1eb7abf4a488d2ad5ca8d3692985ef8a8146d
potato03kth <potatokth@gmail.com>
2021-10-29 16:14:19 +0900

    asdf

--- 2/app.js
+++ 2/app.js
@@ -1,6 +1,10 @@
 var size = document.querySelector("header");
 
 var x = window.innerWidth;
+//!?
+//!
+// todo
+////asdfasdf///////
 
 window.onresize = function (event) {
   var innerWidth = window.innerWidth;
