cd418cc34bce597cebf4d312efb50b486074ae13
potato03kth <potatokth@gmail.com>
2022-01-13 13:26:57 +0900

    recap

--- 1/superEventHandler.js
+++ 1/superEventHandler.js
@@ -1,26 +1,33 @@
 const dlc = document.querySelector(".day-light-copy");
 alert(dlc.innerHTML);
 
-function handlerClick() {
-  dlc.innerHTML = "Oh, what was that? stop click me!";
-}
-function handlerHover() {
-  dlc.innerHTML = "wanna click me now?";
-}
-function handlerPageResize() {
-  dlc.innerHTML = "You Resized my house!";
-  document.getElementsByClassName("day-light-copy").style.color = "blue";
-}
-function handlerHoverOut() {
-  dlc.innerHTML = "Oh, you don't wanna bodrting me now!";
-}
-function handlerClickRight() {
-  dlc.innerHTML = "Ouch!";
-  document.getElementsByClassName("day-light-copy").style.color = "Red";
+function superEventHandler(a) {
+  dlc.innerHTML = "큰거온다...";
+  console.log(a);
 }
 
-dlc.addEventListener("click", handlerClick);
-dlc.addEventListener("onmouseover", handlerHover);
-dlc.addEventListener("onmouseout", handlerHoverOut);
-dlc.addEventListener("resize", handlerPageResize);
-dlc.addEventListener("contextmenu", handlerClickRight);
+dlc.addEventListener("click", superEventHandler() { innerHTML = "bigger from event Listener"; });
+
+// function handlerClick() {
+//   dlc.innerHTML = "Oh, what was that? stop click me!";
+// }
+// function handlerHover() {
+//   dlc.innerHTML = "wanna click me now?";
+// }
+// function handlerPageResize() {
+//   dlc.innerHTML = "You Resized my house!";
+//   document.getElementsByClassName("day-light-copy").style.color = "blue";
+// }
+// function handlerHoverOut() {
+//   dlc.innerHTML = "Oh, you don't wanna bodrting me now!";
+// }
+// function handlerClickRight() {
+//   dlc.innerHTML = "Ouch!";
+//   document.getElementsByClassName("day-light-copy").style.color = "Red";
+// }
+
+// dlc.addEventListener("click", handlerClick);
+// dlc.addEventListener("onmouseover", handlerHover);
+// dlc.addEventListener("onmouseout", handlerHoverOut);
+// dlc.addEventListener("resize", handlerPageResize);
+// dlc.addEventListener("contextmenu", handlerClickRight);
