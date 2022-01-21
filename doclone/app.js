c11aae86b625233126c21779b4c820c752079a57
potato03kth <potatokth@gmail.com>
2022-01-21 19:40:05 +0900

    local-memorization

--- doclone/app.js
+++ doclone/app.js
@@ -1,22 +1,29 @@
 const loginForm = document.querySelector(".login-formular");
 const anker = document.querySelector("body div a");
+const head = document.querySelector("body h1");
 
-function onLoginBtnClick() {
-  //   console.dir(loginInput);
-  //   console.log("ein Klicken!");
+const HIDDEN_CLASSNAME = "hidden";
+const USERNAME_KEY = "username";
+
+const savedUsername = localStorage.getItem(USERNAME_KEY);
+
+loginForm.classList.add(HIDDEN_CLASSNAME);
+head.classList.add(HIDDEN_CLASSNAME);
+
+if (savedUsername === null) {
+  loginForm.classList.remove(HIDDEN_CLASSNAME);
+  loginForm.addEventListener("submit", onLoginFormSubmit);
+} else {
+  head.classList.remove(HIDDEN_CLASSNAME);
+  head.innerHTML = `Willkommen, ${savedUsername}`;
 }
 
 function onLoginFormSubmit(potato) {
   potato.preventDefault();
   console.log(loginForm.querySelector("input").value);
+  const username = loginForm.querySelector("input").value;
+  loginForm.classList.add(HIDDEN_CLASSNAME);
+  head.classList.remove(HIDDEN_CLASSNAME);
+  head.innerHTML = `Willkommen, ${username}`;
+  localStorage.setItem(USERNAME_KEY, username);
 }
-
-function onAnkerClick(event) {
-  event.preventDefault();
-  //   alert("kann sich nicht nähern", anker.href);
-  console.log('kann sich nicht nähern"', anker.href, '"');
-  console.log(event);
-}
-
-anker.addEventListener("click", onAnkerClick);
-loginForm.addEventListener("submit", onLoginFormSubmit);
